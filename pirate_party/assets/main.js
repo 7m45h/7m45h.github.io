ffccefad3120b136ff6a56cf52fd1c80d23e9b9d
tmash <132013534+7m45h@users.noreply.github.com>
2023-05-18 23:12:19 +0530

    clean up

--- pirate_party/assets/main.js
+++ pirate_party/assets/main.js
@@ -1,9 +1,1 @@
-async function setPlaceHolder() {
-    placeHolder = await navigator.mediaDevices.getUserMedia({ video: { width: 256, height: 144 }, audio: false });
-    document.getElementById("cam-1").srcObject = placeHolder;
-    document.getElementById("cam-2").srcObject = placeHolder;
-    document.getElementById("cam-3").srcObject = placeHolder;
-    document.getElementById("cam-4").srcObject = placeHolder;
-}
 
-setPlaceHolder();
