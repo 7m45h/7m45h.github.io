9544b500e4abc33eec3cbb4ea5825d7bb4e95249
tmash <132013534+7m45h@users.noreply.github.com>
2023-05-18 19:14:30 +0530

    redesign

--- shared/footer.js
+++ shared/footer.js
@@ -1,10 +1,11 @@
 const scrollBar = document.getElementById("div-scroll");
+const mainDiv = document.getElementById("main-div");
 
 function updateScrollBar() {
-    let scroll = (document.documentElement.scrollTop / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 100;
+    let scroll = (mainDiv.scrollTop / (mainDiv.scrollHeight - mainDiv.clientHeight)) * 100;
     scrollBar.style.width = scroll + "%";
 }
 
 updateScrollBar();
 
-window.addEventListener("scroll", updateScrollBar);
+mainDiv.addEventListener("scroll", updateScrollBar);
