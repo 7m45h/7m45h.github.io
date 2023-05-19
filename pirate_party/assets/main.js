ae1958f8f02f1529414564770d10578a69f2cf45
tmash <132013534+7m45h@users.noreply.github.com>
2023-05-19 18:16:27 +0530

    cleanup

--- pirate_party/assets/main.js
+++ pirate_party/assets/main.js
@@ -1,8 +1,1 @@
-async function setPlaceHolder() {
-    let stream = await navigator.mediaDevices.getUserMedia({ video: { height: 320, width: 240 }, audio: false });
-    document.querySelectorAll("#div-cams > video").forEach((video) => {
-        video.srcObject = stream;
-    });
-}
 
-setPlaceHolder();
