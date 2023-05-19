1943855415f7852b5949a5c0db6d5a0c4000b527
tmash <132013534+7m45h@users.noreply.github.com>
2023-05-19 15:27:11 +0530

    clean up

--- shared/footer.js
+++ shared/footer.js
@@ -1,10 +1,1 @@
-const footer = document.getElementById("footer");
-const mainDiv = document.getElementById("div-main");
 
-function updateScrollBar() {
-    footer.style.width = (mainDiv.scrollTop / (mainDiv.scrollHeight - mainDiv.clientHeight)) * 100 + "%";
-}
-
-updateScrollBar();
-
-mainDiv.addEventListener("scroll", updateScrollBar);
