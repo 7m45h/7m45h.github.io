3bfa9a8a3c0dd9bf5f63ce806e7fd73fa5ccee54
tmash <>
2023-05-28 08:47:46 +0000

    move to a new repo

--- pirate_party/assets/footer.js
+++
@@ -1,8 +0,0 @@
-const scrollElm = document.getElementById("div-cams");
-const footer = document.getElementById("footer");
-
-function updateFooter() {
-    footer.style.width = (scrollElm.scrollTop / (scrollElm.scrollHeight - scrollElm.clientHeight)) * 100 + "%";
-}
-
-scrollElm.addEventListener("scroll", updateFooter);
