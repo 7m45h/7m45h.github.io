5af7848cf4f207007edaaff41aca51bb9ee52523
tmash <132013534+7m45h@users.noreply.github.com>
2023-06-24 23:12:57 +0530

    base evil lisr

--- blog/evil_list/assets/main.js
+++
@@ -1,8 +0,0 @@
-const main = document.getElementById("main");
-const footer = document.getElementById("footer");
-
-function updateFooter() {
-    footer.style.width = (main.scrollTop / (main.scrollHeight - main.clientHeight)) * 100 + "%";
-}
-
-main.addEventListener("scroll", updateFooter);
