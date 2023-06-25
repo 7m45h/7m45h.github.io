c84254d1c7821c53558d8fe113fe003c6afdd039
tmash <132013534+7m45h@users.noreply.github.com>
2023-06-25 13:07:22 +0530

    2.0

--- shared/scripts/base.js
+++ shared/scripts/base.js
@@ -5,10 +5,10 @@ const _navBar = document.getElementById("nav-main");
 // toggle nav element in pages when mouse touchs the left edge
 window.addEventListener('mousemove', (evt) => {
     if (evt.clientX == 0) {
-        _navBar.style.width = '20%';
+        _navBar.style.width = '20vw';
         _navBar.style.opacity = '1';
-    } else if (evt.clientX > window.innerWidth * 20 / 100) {
+    } else if (evt.clientX > window.innerWidth * 0.2) {
         _navBar.style.opacity = '0';
-        _navBar.style.width = '0px';
+        _navBar.style.width = '0vw';
     }
 });
