f1ad1390dbe0ffdeec936f5b9b23b9a19810a820
tmash <132013534+7m45h@users.noreply.github.com>
2023-06-29 16:26:35 +0530

    much better

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,19 @@
 // UI
 
 const _divHome = document.querySelector("#div-home");
-const _randImg = document.querySelector("#div-home img");
 
 async function setRandImg() {
     let randId = Math.random();
     let randImgInfo;
 
-    _randImg.src = `https://picsum.photos/seed/${randId}/1920/1080`;
     randImgInfo = await fetch(`https://picsum.photos/seed/${randId}//info`);
     randImgInfo = await randImgInfo.json();
 
-    _divHome.innerHTML += `<a class="anc-ext" href="${randImgInfo.url}" target="_blank">&#169;${randImgInfo.author}  &#8599;</a>`
+    _divHome.innerHTML += `
+		<img src="https://picsum.photos/seed/${randId}/1920/1080" title="Random image from awsome site picsum.photos">
+        <a class="anc-ext" href="https://github.com/DMarby/picsum-photos" target="_blank">&#169;picsum.photos  &#8599;</a>
+		<a class="anc-ext" href="${randImgInfo.url}" target="_blank">&#169;${randImgInfo.author}  &#8599;</a>
+	`
 }
 
 setRandImg();
