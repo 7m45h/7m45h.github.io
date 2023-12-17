bc8ce11d022291868f0f36e3e7dcf57ac7de3dc1
tmash <132013534+7m45h@users.noreply.github.com>
2023-12-17 11:52:09 +0530

    complete: p nav-bar nav-menu | start r-panel img section

--- shared/scripts/nav_bar.js
+++ shared/scripts/nav_bar.js
@@ -1,18 +1,15 @@
 const navBar = document.getElementById("nav-bar");
-const mainEL = document.querySelector("main");
 
 let navIsOpen = false;
 
 window.addEventListener("mousemove", (event) => {
-  if ( !navIsOpen && event.clientX < window.innerWidth * 0.2 ) {
+  if ( !navIsOpen && event.clientX < window.innerWidth * 0.1 ) {
     navBar.style.opacity = 1;
-    navBar.style.width = "20vw";
-    mainEL.style.width = "50%";
+    navBar.style.width = "25vw";
     navIsOpen = true;
-  } else if ( navIsOpen && event.clientX > window.innerWidth * 0.3 ) {
+  } else if ( navIsOpen && event.clientX > window.innerWidth * 0.2 ) {
     navBar.style.opacity = 0;
     navBar.style.width = "0px";
-    mainEL.style.width = "75%";
     navIsOpen = false;
   }
 });
--- shared/scripts/r_panel.js
+++ shared/scripts/r_panel.js
@@ -3,15 +3,13 @@ const rPanel = document.getElementById("r-panel");
 let rPanelIsOpen = false;
 
 window.addEventListener("mousemove", (event) => {
-  if ( !rPanelIsOpen && event.clientX > window.innerWidth * 0.8 ) {
+  if ( !rPanelIsOpen && event.clientX > window.innerWidth * 0.9 ) {
     rPanel.style.opacity = 1;
-    rPanel.style.width = "20vw";
-    mainEL.style.width = "50%";
+    rPanel.style.width = "25vw";
     rPanelIsOpen = true;
   } else if ( rPanelIsOpen && event.clientX < window.innerWidth * 0.8 ) {
     rPanel.style.opacity = 0;
     rPanel.style.width = "0px";
-    mainEL.style.width = "75%";
     rPanelIsOpen = false;
   }
 });
