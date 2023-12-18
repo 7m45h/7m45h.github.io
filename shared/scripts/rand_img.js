cff281072caaa933cf62a682568e58ea4a465d81
tmash <132013534+7m45h@users.noreply.github.com>
2023-12-18 14:30:52 +0530

    fix: rand_img.js

--- shared/scripts/rand_img.js
+++ shared/scripts/rand_img.js
@@ -4,11 +4,11 @@ async function setRandIMG(randIMG) {
   let randID = Math.random();
   let imgInfo;
 
-  imgInfo = await fetch(`https://picsum.photos/seed/${randID}//info`);
+  imgInfo = await fetch(`https://picsum.photos/seed/${randID}/info`);
   imgInfo = await imgInfo.json();
 
   randIMG.innerHTML = `
-    <img src="https://picsum.photos/seed/${randID}/1280/720/" alt="random image from awsome site picsum.photos" loading="lazy">
+    <img src="${imgInfo.download_url}" alt="random image from awsome site picsum.photos" loading="lazy">
     <figcaption>
       <a class="anc-ext" href="https://github.com/DMarby/picsum-photos" target="_blank">picsum.photos</a>
       <a class="anc-ext" href="${imgInfo.url}" target="_blank">${imgInfo.author}</a>
