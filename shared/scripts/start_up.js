7459440d3158d64301ebdf7cc8ab82baab49f3e7
tmash <132013534+7m45h@users.noreply.github.com>
2024-02-07 07:13:53 +0530

    clock -> last modified date

--- shared/scripts/start_up.js
+++ shared/scripts/start_up.js
@@ -1,11 +1,8 @@
-import { setTime } from "./clock.js";
 import { prefersDark, setThemeAuto, setThemeManual, themeSwitch } from "./color_manager.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   setThemeAuto();
-  setTime();
 
   prefersDark.addEventListener("change", setThemeAuto);
   themeSwitch.addEventListener("click", setThemeManual);
-  window.setInterval(setTime, 60000);
 });
