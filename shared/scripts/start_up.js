89c361b33c2e3b4384fc1f3a4ebf69674e29b323
tmash <132013534+7m45h@users.noreply.github.com>
2024-02-09 14:07:46 +0530

    no js theme switch

--- shared/scripts/start_up.js
+++
@@ -1,6 +0,0 @@
-import { prefersDark, setThemeAuto, setThemeManual, themeSwitch } from "./color_manager.js";
-
-setThemeAuto();
-
-prefersDark.addEventListener("change", setThemeAuto);
-themeSwitch.addEventListener("click", setThemeManual);
