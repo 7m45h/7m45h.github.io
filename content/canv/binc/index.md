fb0d770060e029d384e56e3c0d145bd6e2eb630d
tmash <132013534+7m45h@users.noreply.github.com>
2024-03-06 19:26:01 +0530

    much better

--- content/canv/binc/index.md
+++ content/canv/binc/index.md
@@ -1,11 +1,8 @@
 +++
 title = 'binc'
-description = "description"
-tags = ["tag", "on"]
+description = "binary clock with javascript and html5 canvas"
+tags = ["binary", "clock", "javascript", "html5", "canvas"]
 date = 2024-03-06
-draft = true
+draft = false
 repo = "url"
-[[powerdby]]
-name = "name"
-link = "url"
 +++
--- content/canv/binc/scripts/binc.js
+++ content/canv/binc/scripts/binc.js
@@ -2,68 +2,86 @@ const div_main = document.querySelector("main");
 const canv     = document.querySelector("canvas");
 const ctx      = canv.getContext("2d");
 
-const two_PI        = Math.PI * 2;
-const bit_count_r   = 6;
-const bit_count_c   = 3;
-const bit_size      = 20;
-const bit_gap       = 10;
-const bit_size_two  = bit_size * 2 + bit_gap;
-const grid_width    = bit_size_two * bit_count_r;
-const grid_height   = bit_size_two * bit_count_c;
-const clock = [ [], [], [], ];
+const two_PI              = Math.PI * 2;
+const bit_size_r          = 20;
+const bit_gap             = 10;
+const bit_gap_half        = bit_gap * 0.5;
+const bit_size_d          = bit_size_r * 2 + bit_gap;
+const clock_row_bit_count = 6;
+const clock_col_bit_count = 3;
+const clock_grid_width    = clock_row_bit_count * bit_size_d;
+const clock_grid_height   = clock_col_bit_count * bit_size_d;
 
-let bit_offset_x = 0;
-let bit_offset_y = 0;
+let clock_bit_offset_x = 0;
+let clock_bit_offset_y = 0;
 
-function calc_bit_offset()
+const time_in_bits = ["", "", ""];
+
+function fit_canv()
 {
-  bit_offset_x = canv.width * 0.5 - grid_width * 0.5 + bit_size;
-  bit_offset_y = canv.height * 0.5 - grid_height * 0.5 + bit_size;
+  canv.width  = div_main.clientWidth;
+  canv.height = div_main.clientHeight;
+}
+
+function calc_clock_bit_offset()
+{
+  clock_bit_offset_x = canv.width * 0.5 - clock_grid_width * 0.5 + bit_size_r + bit_gap_half;
+  clock_bit_offset_y = canv.height * 0.5 - clock_grid_height * 0.5 + bit_size_r + bit_gap_half;
+}
+
+function handle_resize()
+{
+  fit_canv();
+  calc_clock_bit_offset();
+  ctx.lineWidth = 2;
+}
+
+function int_to_bits(int)
+{
+  return int.toString(2).padStart(6, 0);
 }
 
 function update_clock()
 {
-  const crnt_time = new Date();
-  clock[0] = crnt_time.getHours().toString(2).padStart(6, 0).split('');
-  clock[1] = crnt_time.getMinutes().toString(2).padStart(6, 0).split('');
-  clock[2] = crnt_time.getSeconds().toString(2).padStart(6, 0).split('');
+  const crnt_date = new Date();
+  time_in_bits[0] = int_to_bits(crnt_date.getHours());
+  time_in_bits[1] = int_to_bits(crnt_date.getMinutes());
+  time_in_bits[2] = int_to_bits(crnt_date.getSeconds());
 }
 
-function draw_clock()
+function render_clock()
 {
   ctx.clearRect(0, 0, canv.width, canv.height);
   ctx.fillStyle   = color_fg;
   ctx.strokeStyle = color_fg;
-  ctx.lineWidth   = 2;
-
-  for (let r = 0; r < bit_count_c; r++)
+  for (let r = 0; r < clock_col_bit_count; r++)
   {
-    for (let c = 0; c < bit_count_r; c++)
+    for (let c = 0; c < clock_row_bit_count; c++)
     {
       ctx.beginPath();
-      ctx.arc((c * bit_size_two) + bit_offset_x, (r * bit_size_two) + bit_offset_y, bit_size, 0, two_PI);
+      ctx.arc(c * bit_size_d + clock_bit_offset_x, r * bit_size_d + clock_bit_offset_y, bit_size_r, 0, two_PI);
       ctx.closePath();
-      ctx.stroke();
-      if (clock[r][c] == "1")
-      {
+      if (time_in_bits[r][c] == "1") {
         ctx.fill();
+      } else {
+        ctx.stroke();
       }
     }
   }
 }
 
-function update_canv_size()
-{
-  canv.width = div_main.clientWidth;
-  canv.height = div_main.clientHeight;
-}
-update_canv_size();
-calc_bit_offset();
 function main()
 {
   update_clock();
-  draw_clock();
+  render_clock();
   window.requestAnimationFrame(main);
 }
 
-window.requestAnimationFrame(main);
+function init()
+{
+  handle_resize()
+  window.addEventListener("resize", handle_resize);
+  window.requestAnimationFrame(main);
+}
+
+init();
