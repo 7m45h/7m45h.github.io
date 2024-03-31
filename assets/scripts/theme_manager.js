dec118d67e127946d4a64c608a9b6c2f6abbf83b
tmash <132013534+7m45h@users.noreply.github.com>
2024-03-31 11:22:32 +0530

    perfect values

--- assets/scripts/theme_manager.js
+++ assets/scripts/theme_manager.js
@@ -6,8 +6,8 @@ document.documentElement.style.setProperty("--color-fb", crnt_colors.fb);
 function set_random_bg_color()
 {
   let rand_h =  Math.round(Math.random() * 360);
-  let rand_s = (Math.round(Math.random() * 40) + 60);
-  let rand_l = (Math.round(Math.random() * 50) + 50);
+  let rand_s = (Math.round(Math.random() * 40) + 60); // 70%
+  let rand_l = (Math.round(Math.random() * 50) + 50); // 60%
   crnt_colors.bg = `hsl(${rand_h}deg, ${rand_s}%, ${rand_l}%)`;
   document.documentElement.style.setProperty("--color-bg", crnt_colors.bg);
 }
